cc960bfa10d90d5ee2ad881a8acc960fb4486ebe
takuyaw-w <takuya.jamtb@gmail.com>
2026-04-28 01:48:38 +0900

    wip

--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
-import { env } from '../../src/config/env.js'
+import { env } from '@/src/config/env.js'
 
 const pool = new Pool({
   connectionString: env.DATABASE_URL
