ab3f679df282fa0aea3d3a42fec30f98f28cd012
takuyaw-w <takuya.jamtb@gmail.com>
2026-04-28 01:41:06 +0900

    wip

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
+import { env } from './src/config/env.js'
 
 export default defineConfig({
   out: './src/db/migration',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!
+    url: env.DATABASE_URL
   }
 })
--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -1,9 +1,9 @@
-import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
+import { env } from '../../src/config/env.js'
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!
+  connectionString: env.DATABASE_URL
 })
 
 export const db = drizzle({ client: pool })
